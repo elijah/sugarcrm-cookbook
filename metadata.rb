22af08fd6aaa0c2aa98bda9c8c17d5074227314d
Elijah Wright <elijah.wright@gmail.com>
2015-06-04 01:30:30 -0500

    xxx

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "wwalter@sugarcrm.com"
 license          "Apache 2.0"
 description      "Installs/Configures SugarCRM CE"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.2"
+version          "0.1.3"
 depends          "php"
 depends          "apache2"
 depends          "mysql"
