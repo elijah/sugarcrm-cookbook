9fbee4c786b9f781d5e228302035db1ecaec8afa
Elijah Wright <elijah.wright@gmail.com>
2015-06-04 02:00:49 -0500

    Sigh.

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,8 +21,10 @@
 include_recipe "apache2"
 include_recipe %w{php::package php::module_mysql}
 include_recipe "git"
-include_recipe 'mysql2_chef_gem'
 
+#include_recipe 'mysql2_chef_gem'
+
+chef_gem "mysql2"
 
 mysql_service 'sugarcrm-mysql' do
   port '3306'
